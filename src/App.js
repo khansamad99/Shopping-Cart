302fc2e817b512f1e9432fe3cb6bd75b5c1499f3
Samad Khan <samadk619@gmail.com>
2020-11-24 13:39:09 +0530

    Added store

--- src/App.js
+++ src/App.js
@@ -5,7 +5,16 @@ import CartContainer from "./components/CartContainer";
 // items
 import cartItems from "./cart-items";
 // redux stuff
+import reducer from './reducers';
+import {createStore} from 'redux';
 
+const initState = {
+  cart:cartItems,
+  total:0,
+  amount:0
+}
+
+const store = createStore(initState,reducer);
 
 function App() {
   // cart setup
---
+++ src/reducers.js
@@ -0,0 +1,13 @@
+import {INCREASE,DECREASE} from './actions';
+
+
+const reducer = (state,action) =>  (
+    switch(action.type){
+        CASE 'INCREASE':{
+            return {...state,count:state.count+1}
+        }
+    }
+)
+
+
+export default reducer;
