87c0f80229d74b64d847a23fa872a416715fff68
Samad Khan <samadk619@gmail.com>
2020-11-25 22:28:40 +0530

    Remove func added

--- src/reducers.js
+++ src/reducers.js
@@ -1,12 +1,14 @@
-import {INCREASE,DECREASE,CLEAR_CART} from './actions';
+import {INCREASE,DECREASE,CLEAR_CART,REMOVE,GET_TOTAL,GET_AMOUNT} from './actions';
 
 
 const reducer = (state,action) =>  {
     switch(action.type){
-        case 'INCREASE':
+        case INCREASE:
             return [...state,state.amount+1];
-        case 'CLEAR_CART':
-            return {...state,cart:[]}
+        case CLEAR_CART:
+            return {...state,cart:[]};
+        case REMOVE:
+            return {...state,cart:state.cart.filter(cartItem => cartItem.id !== action.payload.id)};
         default:
             return state;
         }
