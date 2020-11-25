300843e24eae9a6a842bd13e770533dead1c1424
Samad Khan <samadk619@gmail.com>
2020-11-25 20:30:08 +0530

    Addded clear cart func

--- src/reducers.js
+++ src/reducers.js
@@ -1,10 +1,12 @@
-import {INCREASE,DECREASE} from './actions';
+import {INCREASE,DECREASE,CLEAR_CART} from './actions';
 
 
 const reducer = (state,action) =>  {
     switch(action.type){
         case 'INCREASE':
             return [...state,state.amount+1];
+        case 'CLEAR_CART':
+            return {...state,cart:[]}
         default:
             return state;
         }
